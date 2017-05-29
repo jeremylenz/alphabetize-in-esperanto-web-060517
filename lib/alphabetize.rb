e7b642874758da5dc73210b830337765a84abd08
Jeremy Lenz <jeremylenz@gmail.com>
2017-05-29 15:15:24 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,16 @@
 def alphabetize(arr)
-  # code here
+
+esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+e_chars = esperanto_alphabet.chars
+sorted_array = arr.sort_by {|word| [e_chars.index(word[0]),
+   e_chars.index(word[1]),
+    e_chars.index(word[2]),
+     e_chars.index(word[3]),
+      e_chars.index(word[4]),
+       e_chars.index(word[5]),
+        e_chars.index(word[6])]}
+
+
+
+
 end
